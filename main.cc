d06f4136fceca19ff2a55a0d48ab4549609ad38e
GliGli <gligli@sfxteam.org>
2014-08-23 16:44:46 +0200

    RAM hex dump using 'R'

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <signal.h>
+#include <ctype.h>
 #include "mc6850.h"
 #include "mc6809.h"
 
@@ -308,6 +309,55 @@ void PrintMidiDevices()
 	}
 }
 
+#ifndef HEXDUMP_COLS
+#define HEXDUMP_COLS 16
+#endif
+ 
+void hexdump(void *mem, unsigned int len)
+{
+        unsigned int i, j;
+        
+        for(i = 0; i < len + ((len % HEXDUMP_COLS) ? (HEXDUMP_COLS - len % HEXDUMP_COLS) : 0); i++)
+        {
+                /* print offset */
+                if(i % HEXDUMP_COLS == 0)
+                {
+                        printf("0x%06x: ", i);
+                }
+ 
+                /* print hex data */
+                if(i < len)
+                {
+                        printf("%02x ", 0xFF & ((char*)mem)[i]);
+                }
+                else /* end of block, just aligning for ASCII dump */
+                {
+                        printf("   ");
+                }
+                
+                /* print ASCII dump */
+                if(i % HEXDUMP_COLS == (HEXDUMP_COLS - 1))
+                {
+                        for(j = i - (HEXDUMP_COLS - 1); j <= i; j++)
+                        {
+                                if(j >= len) /* end of block, not really printing */
+                                {
+                                        putchar(' ');
+                                }
+                                else if(isprint(((char*)mem)[j])) /* printable char */
+                                {
+                                        putchar(0xFF & ((char*)mem)[j]);        
+                                }
+                                else /* other char */
+                                {
+                                        putchar('.');
+                                }
+                        }
+                        putchar('\n');
+                }
+        }
+}
+
 int main(int argc, char *argv[])
 {
 	printf("-= Matrix 1000 emulator =-\n");
@@ -320,6 +370,7 @@ int main(int argc, char *argv[])
 	printf("\tD : Toggle DCOs infos\n");
 	printf("\tC : Toggle CVs infos\n");
 	printf("\tL : Toggle LEDs infos\n");
+	printf("\tR : Dump RAM\n");
 	
 	PrintMidiDevices();
 	
@@ -469,6 +520,12 @@ void sys::status(void)
 		if(GetAsyncKeyState('C')&0x8000)
 		{
 			dCV=!dCV;
+			Sleep(200);
+		}
+
+		if(GetAsyncKeyState('R')&0x8000)
+		{
+			hexdump(&memory[0x0000],0x2000);
 			Sleep(200);
 		}
 
